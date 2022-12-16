6accd1eb2f5e0e8c40ce3fc3adb9eb3cc9efe70c
Captain Awesome Jnr <90992828+captainawesomejnr@users.noreply.github.com>
2022-12-16 17:22:03 +1100

    New startup and credits

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # This checks your code.
 def CodeCheck():
-    if Current_code == Combo:
-        basic.show_number(Secret)
-        serial.write_line("Here's the password:" + str(Secret))
+    if len(Current_code) == 6:
+        if Current_code == Combo:
+            basic.show_number(Secret)
+            serial.write_line("Here's the password:" + ("" + str(Secret)))
+        else:
+            basic.show_icon(IconNames.NO)
+            control.reset()
 
 def on_button_pressed_a():
     global Temporary, Current_code
     Temporary = "" + Current_code + "A"
     Current_code = Temporary
-    serial.write_line(Current_code)
+    serial.write_line("" + (Current_code))
     CodeCheck()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
@@ -16,7 +20,7 @@ def on_button_pressed_b():
     global Temporary, Current_code
     Temporary = "" + Current_code + "B"
     Current_code = Temporary
-    serial.write_line(Current_code)
+    serial.write_line("" + (Current_code))
     CodeCheck()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
@@ -24,8 +28,62 @@ Temporary = ""
 Current_code = ""
 Combo = ""
 Secret = 0
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+""")
 # Your secret goes here
 Secret = 3385
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        # . . . .
+        . . . . .
+        . . . . .
+""")
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        # # . . .
+        . . . . .
+        . . . . .
+""")
 # The password needed; make sure it is only a's and b's.
 Combo = "BAABAA"
+basic.pause(1000)
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        # # # . .
+        . . . . .
+        . . . . .
+""")
 Current_code = ""
+basic.pause(1000)
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        # # # # .
+        . . . . .
+        . . . . .
+""")
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        # # # # #
+        . . . . .
+        . . . . .
+""")
+basic.show_leds("""
+    . . # . .
+        . # . # .
+        . # . # .
+        . # # # .
+        . # # # .
+""")
+basic.pause(5000)
+basic.show_string("Captain Awesome Jnr’s MICROBIT CODE LOCKER")
+basic.pause(1000)
--- main.ts
+++ main.ts
@@ -1,28 +1,87 @@
 // This checks your code.
 function CodeCheck () {
-    if (Current_code == Combo) {
-        basic.showNumber(Secret)
-        serial.writeLine("Here's the password:" + ("" + Secret))
+    if (Current_code.length == 6) {
+        if (Current_code == Combo) {
+            basic.showNumber(Secret)
+            serial.writeLine("Here's the password:" + ("" + Secret))
+        } else {
+            basic.showIcon(IconNames.No)
+            control.reset()
+        }
     }
 }
 input.onButtonPressed(Button.A, function () {
     Temporary = "" + Current_code + "A"
     Current_code = Temporary
-    serial.writeLine(Current_code)
+    serial.writeLine("" + (Current_code))
     CodeCheck()
 })
 input.onButtonPressed(Button.B, function () {
     Temporary = "" + Current_code + "B"
     Current_code = Temporary
-    serial.writeLine(Current_code)
+    serial.writeLine("" + (Current_code))
     CodeCheck()
 })
 let Temporary = ""
 let Current_code = ""
 let Combo = ""
 let Secret = 0
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
 // Your secret goes here
 Secret = 3385
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
 // The password needed; make sure it is only a's and b's.
 Combo = "BAABAA"
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # # . .
+    . . . . .
+    . . . . .
+    `)
 Current_code = ""
+basic.pause(1000)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # # # .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    # # # # #
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    . # . # .
+    . # # # .
+    . # # # .
+    `)
+basic.pause(5000)
+basic.showString("Captain Awesome Jnr’s MICROBIT CODE LOCKER")
+basic.pause(1000)
